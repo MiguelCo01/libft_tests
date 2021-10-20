b87bfb81f6b42d9c7b20926e13a8f52f9ab99a0b
mmelo-da <miguelmelocosta@gmail.com>
2021-10-21 00:41:00 +0100

    add test for b

--- runner.c
+++ runner.c
@@ -1,46 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha_tests.c                                 :+:      :+:    :+:   */
+/*   runner.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmelo-da <mmelo-da@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 23:16:13 by mmelo-da          #+#    #+#             */
-/*   Updated: 2021/10/21 00:20:25 by mmelo-da         ###   ########lyon.fr   */
+/*   Updated: 2021/10/21 00:17:22 by mmelo-da         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_libft.h"
-#include "stdio.h"
-#include "assert.h"
-#include "ctype.h"
+#include "libft_tests.h"
 
-static int	g_test_number;
-
-void	_verify_ft_isalpha(char c)
+int	main(void)
 {
-	int	expected;
-	int	actual;
-
-	g_test_number = g_test_number + 1;
-	expected = isalpha(c);
-	actual = ft_isalpha(c);
-	if (expected == actual)
-	{
-		printf("%d - OK\n", g_test_number);
-	}
-	else
-	{
-		printf("%d - NOK - When: char is %c Expected: %d Recieved: %d\n",
-			g_test_number, c, expected, actual);
-	}
-}
-
-int	test_isalpha(void)
-{
-	g_test_number = 0;
-	printf("Testing ft_isalpha...\n");
-	_verify_ft_isalpha('a');
-	_verify_ft_isalpha('b');
+	test_isalpha();
 	return (0);
 }
